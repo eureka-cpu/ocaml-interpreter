29dc8a670476fa7273d6cf1d13b67ae1916eee56
Chris O'Brien <obrchr96@gmail.com>
2025-11-08 22:20:41 -0500

    tests for exercises from ocaml.org

--- test/test_ocaml_interpreter.ml
+++ test/test_ocaml_interpreter.ml
@@ -54,4 +54,43 @@ test "length_tr" (fun () -> assert (length_tr [ "a"; "b"; "c" ] = 3));
 test "length_tr_empty" (fun () -> assert (length_tr [] = 0))
 ;;
 
+(* Reverse a list.
+  OCaml standard library has `List.rev` but we ask that you reimplement it. *)
+let rec aux acc = function
+  | [] -> acc
+  | head :: tail -> aux (head :: acc) tail
+and rev_tr list = aux [] list in
+test "rev" (fun () -> assert (rev_tr [ "a"; "b"; "c"; "d" ] = [ "d"; "c"; "b"; "a" ]))
+;;
+
+let is_palendrome list = List.rev list = list in
+test "is_palendrome" (fun () -> assert (is_palendrome [ "a"; "b"; "b"; "a" ]));
+test "is_not_palendrome" (fun () -> assert (not (is_palendrome [ "a"; "b"; "c"; "d" ])))
+
+(* If you need so, refresh your memory about [run-length encoding](http://en.wikipedia.org/wiki/Run-length_encoding)
+  `inc` is the incrementor for the number of consecutive occurrances of the same data, and `acc` is the accumulator
+  which is where we store the pairs of data where the first element of the tuple is `inc` and the second is the data *)
+type 'a rle =
+  | One of 'a
+  | Many of int * 'a
+
+let new_rle len elem = if len = 1 then One elem else Many (len, elem);;
+
+let rec rle_encode_aux inc acc = function
+  | [] -> [] (* The list is empty *)
+  | [ head ] ->
+    new_rle (inc + 1) head :: acc (* The list contains only one of this element *)
+  | head :: (next_head :: _ as tail) ->
+    (* compare the current head to the next to increase count *)
+    if head = next_head
+    then rle_encode_aux (inc + 1) acc tail
+    else rle_encode_aux 0 (new_rle (inc + 1) head :: acc) tail
+and rle_encode_list_tr list = List.rev (rle_encode_aux 0 [] list) in
+test "rle_encode_list" (fun () ->
+  assert (
+    rle_encode_list_tr
+      [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e" ]
+    = [ Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e") ]))
+;;
+
 print_endline "\nAll tests completed."
